d2eab5d31b0ee7a38a0dafae257bf3cfa9b3f4b8
SaharG <saharg92@gmail.com>
2021-10-30 21:46:25 +0300

    refurbished

--- project_scraper.py
+++ project_scraper.py
@@ -1,22 +1,39 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import re
+import pandas as pd
 
 NUMBER_OF_REVIEWS = 0
-REVIEW = 0
+REVIEWS = 0
+SIZE_OF_REVIEW = 3
+FIRST_RESTAURANT = 1
+TYPE_OF_REST = 0
+PRICE = 1
 
 driver = webdriver.Chrome()
 driver.get('https://www.tripadvisor.com/Restaurants-g293984-Tel_Aviv_Tel_Aviv_District.html')
 html_text = BeautifulSoup(driver.page_source, 'html.parser')
 driver.quit()
 
-rest_names_html = html_text.find_all('a', class_='bHGqj Cj b')
-num_of_reviews_html = html_text.find_all('a', class_='dMdkg _S')
-avg_review_html = html_text.find_all('svg', class_='RWYkj d H0', viewbox="0 0 88 16")
+content = html_text.find_all('div', class_='emrzT Vt o')
+list_of_rest = []
+for restaurant in content[FIRST_RESTAURANT:]:
+    rest_name = restaurant.find('a', class_='bHGqj Cj b').text.lstrip('0123456789. ')
+    avg_review_text = restaurant.find('svg', class_='RWYkj d H0')
+    avg_review = float(avg_review_text.get('title')[:SIZE_OF_REVIEW])
+    num_of_reviews = restaurant.find('span', class_='NoCoR').text.split()[REVIEWS]
+    reviews_type_price = restaurant.find('div', class_='bhDlF bPJHV eQXRG').find_all('span', class_='XNMDG')
+    food_type = reviews_type_price[TYPE_OF_REST].text
+    price = reviews_type_price[PRICE].text
 
-rest_names = [x.text.lstrip('0123456789. ') for x in rest_names_html]
-num_of_reviews = [int(re.findall('[0-9]+', x.text)[NUMBER_OF_REVIEWS]) for x in num_of_reviews_html]
-avg_review = [float(x.get('title').split()[REVIEW]) for x in avg_review_html]
+    table = {'Restaurant name': rest_name,
+             'Review': avg_review,
+             'Number of reviews': num_of_reviews,
+             'Restaurant type': food_type,
+             'Price': price
+             }
 
-for i, rest in enumerate(rest_names):
-    print((rest, num_of_reviews[i], avg_review[i]))
+    list_of_rest.append(table)
+
+data = pd.DataFrame(list_of_rest)
+print(data)
+data.to_csv('Restaurants.csv')
