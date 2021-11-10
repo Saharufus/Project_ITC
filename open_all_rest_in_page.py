33ec3bd0453f8931fd73e6cf57af771261c2ea96
SaharG <saharg92@gmail.com>
2021-11-10 16:25:59 +0200

    working correctly

---
+++ open_all_rest_in_page.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import threading
+
+
+def get_rest_html(url, empty_list):
+    """
+    :param url: url of webpage
+    :param empty_list: A soup object from the url page will be appended to the empty list
+    """
+    driver = webdriver.Chrome()
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.close()
+    empty_list.append(soup)
+
+
+def get_list_of_soups(url_list, empty_list):
+    """
+    :param url_list: A list of urls to extract soups from
+    :param empty_list: The list will be filled with soups of the urls from url_list
+    """
+    thread_list = [threading.Thread(target=get_rest_html, args=(url, empty_list)) for url in url_list]
+
+    for thread in thread_list:
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
