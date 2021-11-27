21680e59adb610285c1aab5af8dd5dc6145d869c
Omer Cohen <omer205@gmail.com>
2021-11-27 20:39:04 +0200

    added get_reviews.py which contains function for retrieving reviews from soup

---
+++ get_reviews.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import time
+import re
+from datetime import datetime
+
+def get_reviews_from_soup(soup):
+    """
+    @param soup: html text from tripadvisor website
+    @return: dictionary with all reviews on html, keys: id, title, user_id, text( review content), rating, date
+    """
+    comments = soup.findAll('div', class_="reviewSelector")
+    reviews_list = []
+    for comment in comments:
+        review_dict = {}
+        review_dict['title'] = comment.find('span', class_='noQuotes').text
+        review_dict['id'] = comment.get('data-reviewid')
+        review_dict['text'] = comment.find('p', class_='partial_entry').text
+        date = comment.find('span', class_='ratingDate').get('title')
+        review_dict['date'] = datetime.strptime(date, '%B %d, %Y')
+        review_dict['user_id'] = comment.find('div', class_="info_text pointer_cursor").text
+        rating = comment.find('div', class_="ui_column is-9").find('span')
+        review_dict['rating'] = int(int(rating['class'][1].split('_')[1]) / 10)
+        reviews_list.append(review_dict)
+    return reviews_list
+
