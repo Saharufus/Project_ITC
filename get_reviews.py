3704d6be3556bb28f29b23ff77eb59752885eba7
Omer Cohen <omer205@gmail.com>
2021-11-28 18:17:34 +0200

    changed names of dict keys

--- get_reviews.py
+++ get_reviews.py
@@ -16,14 +16,14 @@ def get_reviews_from_soup(soup):
     reviews_list = []
     for comment in comments:
         review_dict = {}
-        review_dict['title'] = comment.find('span', class_='noQuotes').text
-        review_dict['id'] = comment.get('data-reviewid')
-        review_dict['text'] = comment.find('p', class_='partial_entry').text
+        review_dict['review_title'] = comment.find('span', class_='noQuotes').text
+        review_dict['rev_id'] = comment.get('data-reviewid')
+        review_dict['review_text'] = comment.find('p', class_='partial_entry').text
         date = comment.find('span', class_='ratingDate').get('title')
         review_dict['date'] = datetime.strptime(date, '%B %d, %Y')
-        review_dict['user_id'] = comment.find('div', class_="info_text pointer_cursor").text
+        review_dict['user_name'] = comment.find('div', class_="info_text pointer_cursor").text
         rating = comment.find('div', class_="ui_column is-9").find('span')
-        review_dict['rating'] = int(int(rating['class'][1].split('_')[1]) / NUM_TO_DIVIDE_RATING)
+        review_dict['rate'] = int(int(rating['class'][1].split('_')[1]) / NUM_TO_DIVIDE_RATING)
         reviews_list.append(review_dict)
     return reviews_list
 
